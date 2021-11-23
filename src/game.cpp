ad478c4d2c3c9ead74bd1d60392b9605834be0a6
barfran <94394730+barfran@users.noreply.github.com>
2021-11-23 12:16:42 +0100

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -29,6 +29,10 @@ void jeu() {
         if(new_nombre == 4) {nb_quatre += 1;}
         tab = ajoute_nombre_plateau(tab, new_nombre);
      }
+    if (cin == "quit"){
+        cout << "fin de partie" << endl
+          quit()
+    }
   }
   affiche_plateau(plateau_to_string(tab));
   cout << "Fin de partie" << endl;
