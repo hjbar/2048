f9d130c18b1971fdf3ceb12fde03823e0c708833
hjbar <hjbar@protonmail.com>
2021-11-27 00:31:03 +0100

    Ajout couleur dans la console

--- include/affichage.hpp
+++ include/affichage.hpp
@@ -4,10 +4,27 @@
 #include <sstream>
 #include <vector>
 #include <fmt/core.h>
+#include <string>
 
 using namespace std;
 
 typedef vector<vector<int>> Plateau;
+
+#define white "\033[0;97m"
+#define red "\033[0;31m"
+#define green "\033[0;32m"
+#define yellow "\033[0;33m"
+#define blue "\033[0;34m"
+#define purple "\033[0;35m"
+#define cyan "\033[0;36m"
+#define reset "\033[0m"
+
+
+/** Fonction qui renvoie la chaine de caracteres permettant d'afficher les nombres en couleur dans la console
+ * @param nombre un nombre entier que l'on veut afficher dans une certaine couleur
+ * @return une string correspond a la bonne couleur associee au nombre en parametre
+ **/
+string couleur_nombre(int nombre);
 
 
 /** Fonction qui tranforme le plateau de jeu en chaine de cracteres
--- src/affichage.cpp
+++ src/affichage.cpp
@@ -1,17 +1,35 @@
 #include "affichage.hpp"
 
 
+string couleur_nombre(int nombre) {
+   if(nombre <= 4) {
+      return green;
+   } else if(nombre <= 16) {
+      return purple;
+   } else if(nombre <= 64) {
+      return blue;
+   } else if(nombre <= 512) {
+      return red;
+   } else if(nombre <= 2048) {
+      return yellow;
+   } else {
+      return cyan;
+   }
+}
+
+
 string plateau_to_string(Plateau t) {
    ostringstream os;
-   os << "*************************" << endl;
+   os << white "*************************" reset << endl;
    for(vector<int> ligne : t) {
-      os << "*";
+      os << white "*" reset;
       for(int nombre : ligne) {
          // On utilise la biblioteque fmt/core.h presente dans le standard de c++ 2020 qui aligne les nombres correctement sur un format de 5 characteres
          string s = nombre != 0 ? fmt::format("{:^5}", nombre) : "     ";
-         os << s << "*";
+         os << couleur_nombre(nombre);
+         os << s << white "*" reset;
       }
-      os << endl << "*************************" << endl;
+      os << endl << white "*************************" reset << endl;
    }
    return os.str();
 }
